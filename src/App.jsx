845d5ee8891d83cc35e1ceb2f1b8bacfeed65f04
srikk <122169588+py-code314@users.noreply.github.com>
2026-07-08 16:59:17 -0400

    feat: Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 // import NavBar from "./components/layouts/navbar/Navbar"
-import Header from "./components/layouts/header/Header"
+import Header from './components/layouts/header/Header'
+import Footer from './components/layouts/footer/Footer'
 function App() {
   return (
     <div className="home-page">
       <Header />
-      {/* <NavBar/> */}
+      <Footer />
     </div>
   )
 }
