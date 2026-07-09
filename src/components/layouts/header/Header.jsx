743cd56bceda5d12536fdc2c51f8a731f902d0ac
srikk <122169588+py-code314@users.noreply.github.com>
2026-07-09 14:34:27 -0400

    style: Add styles to Header

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -1,17 +1,14 @@
 import styles from './Header.module.css'
 import NavBar from '../navbar/Navbar'
 
-
 /* Display Header */
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.headerContent}>
-        {/* Title */}
-        <h1 className={styles.title}>Scriblr</h1>
+      {/* Title */}
+      <h1 className={styles.title}>Scriblr</h1>
 
-        <NavBar />
-      </div>
+      <NavBar />
     </header>
   )
 }
