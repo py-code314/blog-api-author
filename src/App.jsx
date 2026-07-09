de978d4319da7d78d97f5661c414e20e9d5afe11
srikk <122169588+py-code314@users.noreply.github.com>
2026-07-09 08:56:40 -0400

    feat: Add LandingPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 // import NavBar from "./components/layouts/navbar/Navbar"
 // import Header from './components/layouts/header/Header'
 // import Footer from './components/layouts/footer/Footer'
-import HomePageLayout from './components/layouts/homepage/HomePageLayout'
 import { useState } from 'react'
+import HomePageLayout from './components/layouts/homepage/HomePageLayout'
+import LandingPage from './components/pages/landing-page/LandingPage'
 
 function App() {
   const [isSignedIn, setIsSignedIn] = useState(false)
   return (
     <div className="home-page">
       <HomePageLayout>
-        {isSignedIn ? (
-          <p>Show Dashboard</p>
-        ) : (
-          <p>Show Landing Page with Hero section</p>
-        )}
+        {isSignedIn ? <p>Show Dashboard</p> : <LandingPage />}
       </HomePageLayout>
     </div>
   )
---
+++ src/components/pages/landing-page/LandingPage.jsx
@@ -0,0 +1,16 @@
+import styles from './LandingPage.module.css'
+
+const LandingPage = () => {
+  return (
+    <>
+      {/* <Hero /> */}
+      {/* <Footer /> */}
+      <div className={styles.landingPage}>
+        <p>Hero section</p>
+        <p>Footer section</p>
+      </div>
+    </>
+  )
+}
+
+export default LandingPage
