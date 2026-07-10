0fdabb9614cd7520332ce1d4e0493cf41020cdb0
srikk <122169588+py-code314@users.noreply.github.com>
2026-07-10 11:27:08 -0400

    feat: Add a basic sing-up form

---
+++ src/pages/sign-up/SignUp.jsx
@@ -0,0 +1,99 @@
+import styles from './SignUp.module.css'
+
+const SignUp = () => {
+  return (
+    <div className={styles.signUp}>
+      {/* Subtitle */}
+      <h2 className="">Sign Up</h2>
+
+      {/* Sign-up form */}
+      <form
+        className={styles.form}
+        noValidate
+        // onSubmit={handleFormValidation}
+      >
+        {/* Email input */}
+        <div className={styles.formControl}>
+          <label htmlFor="email" className={styles.formLabel}>
+            Email (required)
+          </label>
+          <p className={styles.formHint} id="email-hint">
+            Eg. hello.taylor@example.com
+          </p>
+
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="ericcartman@gmail.com"
+            className={styles.formInput}
+            autoComplete="email"
+            inputMode="email"
+            required
+          />
+        </div>
+
+        {/* Password */}
+        <div className={styles.formControl}>
+          <label className={styles.formLabel} htmlFor="password">
+            Password (required)
+          </label>
+          <p className={styles.passwordReq}>
+            Requires at least 8 characters, one uppercase letter (A - Z), one
+            number (0 - 9), and one special character (!@#$%^&amp;*)
+          </p>
+          <input
+            className={styles.formInput}
+            id="password"
+            name="password"
+            type="password"
+            min={8}
+            placeholder="South^Park97"
+            pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$"
+            required
+          />
+        </div>
+
+        {/* Confirm password */}
+        <div className={styles.formControl}>
+          <label className={styles.formLabel} htmlFor="confirmPassword">
+            Confirm Password (required)
+          </label>
+          <input
+            className={styles.formInput}
+            id="confirmPassword"
+            name="confirmPassword"
+            type="password"
+            min={8}
+            placeholder="South^Park97"
+            pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$"
+            required
+          />
+        </div>
+
+        {/* Name input */}
+        <div className={styles.formControl}>
+          <label htmlFor="name" className={styles.formLabel}>
+            Name (optional)
+          </label>
+
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Eric Cartman"
+            className={styles.formInput}
+            autoComplete="name"
+          />
+        </div>
+
+        {/* Sign up button */}
+        <button className={styles.btn} type="submit">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,17 @@
 import App from '../App'
 // import HomePageLayout from '../components/layouts/homepage/HomePageLayout'
+import SignUp from '../pages/sign-up/SignUp'
 
 /* Array of routes */
 const routes = [
   {
     path: '/',
     element: <App />,
+    
+  },
+  {
+    path: '/sign-up',
+    element: <SignUp />,
     
   },
 ]
