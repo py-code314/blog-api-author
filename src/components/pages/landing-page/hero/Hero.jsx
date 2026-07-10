ff3d3ed03454a716658f235209f549b3e33bbb55
srikk <122169588+py-code314@users.noreply.github.com>
2026-07-10 09:10:56 -0400

    style: Add styles to Hero section

--- src/components/pages/landing-page/hero/Hero.jsx
+++ src/components/pages/landing-page/hero/Hero.jsx
@@ -4,16 +4,16 @@ import styles from './Hero.module.css'
 const Hero = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.contentWrapper}>
-        {/* Subtitle */}
-        <h2 className={styles.subtitle}>Move minds. Share your stories.</h2>
+      {/* Subtitle */}
+      <h2 className={styles.subtitle}>Move minds. Share your stories.</h2>
 
-        <p className={styles.description}>
-          Join a community of modern thinkers, writers, and storytellers. Share
-          your unique perspective with readers who care, and start building your
-          audience today.
-        </p>
-      </div>
+      <p className={styles.description}>
+        Join a community of modern thinkers, writers, and storytellers. Share
+        your unique perspective with readers who care, and start building your
+        audience today.
+      </p>
+
+      <button className={styles.signUpBtn}>Start Writing</button>
     </section>
   )
 }
