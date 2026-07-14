88e718f06326c02ea1278e63558e63a6f88e7010
srikk <122169588+py-code314@users.noreply.github.com>
2026-07-14 19:09:31 -0400

    feat: Validate password and confirm password fields

--- src/pages/sign-up/SignupForm.jsx
+++ src/pages/sign-up/SignupForm.jsx
@@ -23,12 +23,12 @@ const SignupForm = () => {
     confirmPassword: '',
     name: '',
   })
-   const [validInput, setValidInput] = useState({
-     email: false,
-     password: false,
-     confirmPassword: false,
-     name: false,
-   })
+  const [validFormData, setValidFormData] = useState({
+    email: false,
+    password: false,
+    confirmPassword: false,
+    name: false,
+  })
 
   // const handleSignupFormSubmit = (data) => {
   //   setSignupFormData(data)
@@ -54,13 +54,68 @@ const SignupForm = () => {
       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     setBlurredInput({ ...blurredInput, email: true })
     if (!signupFormData.email) {
-      setErrorMessage({...errorMessage, email: 'Please enter your email address'})
-      setValidInput({ ...validInput, email: false })
+      setValidFormData({ ...validFormData, email: false })
+      setErrorMessage({
+        ...errorMessage,
+        email: 'Please enter your email address',
+      })
     } else if (!emailRegExp.test(signupFormData.email)) {
-      setErrorMessage({...errorMessage, email: 'Please enter a valid email address'})
-      setValidInput({ ...validInput, email: false })
+      setValidFormData({ ...validFormData, email: false })
+      setErrorMessage({
+        ...errorMessage,
+        email: 'Please enter a valid email address',
+      })
     } else {
-      setValidInput({ ...validInput, email: true })
+      setValidFormData({ ...validFormData, email: true })
+      setErrorMessage({
+        ...errorMessage,
+        email: '',
+      })
+    }
+  }
+  const handlePasswordValidation = () => {
+    const emailRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/
+    setBlurredInput({ ...blurredInput, password: true })
+    if (!signupFormData.password) {
+      setValidFormData({ ...validFormData, password: false })
+      setErrorMessage({
+        ...errorMessage,
+        password: 'Please enter your password',
+      })
+    } else if (!emailRegExp.test(signupFormData.password)) {
+      setValidFormData({ ...validFormData, password: false })
+      setErrorMessage({
+        ...errorMessage,
+        password: 'Please enter a valid password',
+      })
+    } else {
+      setValidFormData({ ...validFormData, password: true })
+      setErrorMessage({
+        ...errorMessage,
+        password: '',
+      })
+    }
+  }
+  const handleConfirmPasswordValidation = () => {
+    setBlurredInput({ ...blurredInput, confirmPassword: true })
+    if (!signupFormData.confirmPassword) {
+      setValidFormData({ ...validFormData, confirmPassword: false })
+      setErrorMessage({
+        ...errorMessage,
+        confirmPassword: 'Please re-enter your password',
+      })
+    } else if (signupFormData.password !== signupFormData.confirmPassword) {
+      setValidFormData({ ...validFormData, confirmPassword: false })
+      setErrorMessage({
+        ...errorMessage,
+        confirmPassword: 'Passwords do not match',
+      })
+    } else {
+      setValidFormData({ ...validFormData, confirmPassword: true })
+      setErrorMessage({
+        ...errorMessage,
+        confirmPassword: '',
+      })
     }
   }
 
@@ -95,7 +150,7 @@ const SignupForm = () => {
               onChange={handleEmailChange}
               onBlur={handleEmailValidation}
             />
-            {validInput.email && blurredInput.email && (
+            {validFormData.email && blurredInput.email && (
               <img
                 className={styles.formCheckmark}
                 aria-hidden="true"
@@ -106,7 +161,7 @@ const SignupForm = () => {
               />
             )}
           </div>
-          {!validInput.email && blurredInput.email && (
+          {!validFormData.email && blurredInput.email && (
             <div className={styles.formError}>
               <img
                 className={styles.formErrorIcon}
@@ -132,21 +187,53 @@ const SignupForm = () => {
             Password (required)
           </label>
           <p className={styles.passwordReq}>
-            Requires at least 8 characters, one uppercase letter (A - Z), one
-            number (0 - 9), and one special character (!@#$%^&amp;*)
+            Requires at least 8 characters, one lowercase letter (a - z), one
+            uppercase letter (A - Z), one number (0 - 9), and one special
+            character (!@#$%^&amp;*)
           </p>
-          <input
-            className={styles.formInput}
-            id="password"
-            name="password"
-            type="password"
-            min={8}
-            placeholder="South^Park97"
-            pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$"
-            required
-            value={signupFormData.firstName}
-            onChange={handlePasswordChange}
-          />
+          <div className={styles.formValid}>
+            <input
+              className={styles.formInput}
+              id="password"
+              name="password"
+              type="password"
+              min={8}
+              placeholder="South^Park97"
+              pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$"
+              required
+              value={signupFormData.password}
+              onChange={handlePasswordChange}
+              onBlur={handlePasswordValidation}
+            />
+            {validFormData.password && blurredInput.password && (
+              <img
+                className={styles.formCheckmark}
+                aria-hidden="true"
+                src={checkMarkIcon}
+                alt=""
+                width={25}
+                height={25}
+              />
+            )}
+          </div>
+          {!validFormData.password && blurredInput.password && (
+            <div className={styles.formError}>
+              <img
+                className={styles.formErrorIcon}
+                aria-hidden="true"
+                src={errorIcon}
+                alt=""
+                width={25}
+                height={25}
+              />
+              <p
+                className={styles.formErrorMessage}
+                aria-live="polite"
+                id="invalid-password">
+                {errorMessage.password}
+              </p>
+            </div>
+          )}
         </div>
 
         {/* Confirm password */}
@@ -154,18 +241,49 @@ const SignupForm = () => {
           <label className={styles.formLabel} htmlFor="confirmPassword">
             Confirm Password (required)
           </label>
-          <input
-            className={styles.formInput}
-            id="confirmPassword"
-            name="confirmPassword"
-            type="password"
-            min={8}
-            placeholder="South^Park97"
-            pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$"
-            required
-            value={signupFormData.firstName}
-            onChange={handleConfirmPasswordChange}
-          />
+          <div className={styles.formValid}>
+            <input
+              className={styles.formInput}
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              min={8}
+              placeholder="South^Park97"
+              // pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$"
+              required
+              value={signupFormData.confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              onBlur={handleConfirmPasswordValidation}
+            />
+            {validFormData.confirmPassword && blurredInput.confirmPassword && (
+              <img
+                className={styles.formCheckmark}
+                aria-hidden="true"
+                src={checkMarkIcon}
+                alt=""
+                width={25}
+                height={25}
+              />
+            )}
+          </div>
+          {!validFormData.confirmPassword && blurredInput.confirmPassword && (
+            <div className={styles.formError}>
+              <img
+                className={styles.formErrorIcon}
+                aria-hidden="true"
+                src={errorIcon}
+                alt=""
+                width={25}
+                height={25}
+              />
+              <p
+                className={styles.formErrorMessage}
+                aria-live="polite"
+                id="invalid-confirmPassword">
+                {errorMessage.confirmPassword}
+              </p>
+            </div>
+          )}
         </div>
 
         {/* Name input */}
