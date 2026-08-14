6b0593e2fb397158ef9eb5bc63c1712cdbe86d51
srikk <122169588+py-code314@users.noreply.github.com>
2026-08-14 08:53:22 -0400

    fix(profile): Show profile info on page refresh
    Get user data from profile(db) instead of req.user.

--- src/components/pages/homepage/profile/Profile.jsx
+++ src/components/pages/homepage/profile/Profile.jsx
@@ -1,12 +1,9 @@
 import styles from './Profile.module.css'
-import { AuthContext } from '../../../../contexts/auth/AuthContext'
 import { useData } from '../../../../hooks/useData.js'
 import profileIcon from '../../../../assets/icons/icon-profile-2.svg'
-import { useContext } from 'react'
+import errorIcon from '../../../../assets/icons/icon-error-2.svg'
 
 const Profile = () => {
-  const { user } = useContext(AuthContext)
-
   // Get own profile data
   const { data, isLoading, error } = useData(
     'http://localhost:8080/api/v1/profiles/me',
@@ -34,26 +31,22 @@ const Profile = () => {
             height={128}
           />
         </div>
-        <div className={styles.details}>
-          <p>
-            <strong>Name: </strong>
-            {user.name}
-          </p>
-          <p>
-            <strong>Email: </strong>
-            {user.email}
-          </p>
-          <p>
-            <strong>Bio: </strong>
-            🚨 Looks like our servers temporarily forgot who you are!
-          </p>
+        <div className={styles.errorWrapper}>
+          <div className={styles.errorImage}>
+            <img src={errorIcon} alt="" width={70} height={70} />
+          </div>
+
+          <div className={styles.errorContent}>
+            <p>Looks like our servers temporarily forgot who you are!</p>
+            <p>Please try again later.</p>
+          </div>
         </div>
       </div>
     )
 
   // console.log('🚀 ~ Profile ~ data, isLoading, error :', data, isLoading, error)
   const { profile } = data
-  const { bio } = profile
+  const { bio, user } = profile
 
   return (
     <>
