56a33904beac7e597d11ec2e1433a0c8cfbbc432
srikk <122169588+py-code314@users.noreply.github.com>
2026-08-31 09:27:39 -0400

    feat(edit post): Add code to show loading spinner & error msg

--- src/pages/edit-post/EditPost.jsx
+++ src/pages/edit-post/EditPost.jsx
@@ -1,5 +1,71 @@
+/* -------------------- Styles -------------------- */
+import styles from './EditPost.module.css'
+/* -------------------- Hooks -------------------- */
+import { useData } from '../../hooks/useData.js'
+import { useNavigate, useParams } from 'react-router'
+import { useState } from 'react'
+/* -------------------- Components -------------------- */
+import { Link } from 'react-router'
+/* -------------------- Images -------------------- */
+import errorIcon from '../../assets/icons/icon-error-2.svg'
+
 const EditPost = () => {
-  return ( <div>Edit Post</div> );
+  // ? Use code from new post form
+  // TODO: Get post data from db
+  // Populate form with post data
+
+  // TODO: Get id using params
+  // Get post data using that id
+  // * Use code from Post.jsx
+
+  const { id } = useParams()
+
+  // Get a single post data
+  const { data, isLoading, error } = useData(
+    `http://localhost:8080/api/v1/posts/${id}`,
+  )
+
+  // const [deleteError, setDeleteError] = useState(null)
+
+  // Show loading spinner while fetching the data
+  if (isLoading)
+    return (
+      <div className={styles.loaderWrapper}>
+        <div className={styles.loader}>Loading...</div>
+      </div>
+    )
+
+  // Show error message upon failure to fetch the data
+  if (error)
+    return (
+      <div className={styles.editPost}>
+        <title>Scriblr | Edit Post</title>
+
+        <Link className={styles.postLink} to={`/posts/${id}`}>
+          Back to Post 
+        </Link>
+
+        <h2 className={styles.subTitle}>Edit Post</h2>
+        <div className={styles.errorWrapper}>
+          <div className={styles.errorImage}>
+            <img src={errorIcon} alt="" width={60} height={60} />
+          </div>
+
+          <div className={styles.errorContent}>
+            <p>Error retrieving post info. Please try again.</p>
+          </div>
+        </div>
+      </div>
+    )
+
+  const { post } = data
+  const { categories, title, content, published, createdAt, updatedAt, tags } =
+    post
+  return (
+    <>
+      <h2 className={styles.subTitle}>Edit Post</h2>
+    </>
+  )
 }
- 
-export default EditPost;
+
+export default EditPost
--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -124,6 +124,8 @@ const Post = () => {
           Back to All Posts
         </Link>
 
+        {/* // TODO: Add heading 'Post Details' */}
+
         {deleteError && (
           <ErrorContext
             value={{
