553293ef4cb0df40f1c64dd1763648dc19ce3dd5
srikk <122169588+py-code314@users.noreply.github.com>
2026-09-01 17:03:04 -0400

    feat(hero): Show `SignupModal` on clicking `Start Writing` btn

--- src/components/pages/landing-page/hero/Hero.jsx
+++ src/components/pages/landing-page/hero/Hero.jsx
@@ -1,21 +1,44 @@
+/* -------------------- Styles -------------------- */
 import styles from './Hero.module.css'
+/* -------------------- Components -------------------- */
+import Button from '../../../core/Button/Button'
+import SignupModal from '../../../../pages/signup-modal/SignupModal'
+/* -------------------- Context -------------------- */
+import { AuthContext } from '../../../../contexts/auth/AuthContext'
+/* -------------------- Hooks -------------------- */
+import { useContext } from 'react'
 
 /* Display Hero section */
 const Hero = () => {
+  const { activeModal, setActiveModal } = useContext(AuthContext)
+
+  const handleSignup = () => {
+    setActiveModal('signup')
+  }
+
   return (
-    <section className={styles.hero}>
-      {/* Subtitle */}
-      <h2 className={styles.subtitle}>Move minds. Share your stories.</h2>
+    <>
+      <section className={styles.hero}>
+        {/* Subtitle */}
+        <h2 className={styles.subtitle}>Move minds. Share your stories.</h2>
 
-      <p className={styles.description}>
-        Join a community of modern thinkers, writers, and storytellers. Share
-        your unique perspective with readers who care, and start building your
-        audience today.
-      </p>
+        <p className={styles.description}>
+          Join a community of modern thinkers, writers, and storytellers. Share
+          your unique perspective with readers who care, and start building your
+          audience today.
+        </p>
 
-      {/* // TODO: Add functionality to the button */}
-      <button className={styles.signUpBtn}>Start Writing</button>
-    </section>
+        <Button
+          id="startWritingBtn"
+          className="startBtn"
+          title="Sign up"
+          onClick={handleSignup}>
+          Start Writing{' '}
+        </Button>
+      </section>
+
+      {activeModal === 'signup' && <SignupModal />}
+    </>
   )
 }
 
