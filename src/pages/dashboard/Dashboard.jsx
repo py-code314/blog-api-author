d3949700d2e4a941b36f9014e15e0492e0f77108
srikk <122169588+py-code314@users.noreply.github.com>
2026-09-07 11:45:11 -0400

    feat(dashboard): Add link to posts page and style it

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@
 /* -------------------- Components -------------------- */
 import Profile from '../../components/pages/homepage/profile/Profile'
 import RecentPosts from '../../components/pages/homepage/recent-posts/RecentPosts'
+import { Link } from 'react-router'
 
 const Dashboard = () => {
   return (
@@ -9,6 +10,17 @@ const Dashboard = () => {
       <div className={styles.dashboard}>
         <Profile />
         <RecentPosts />
+        <ul>
+          <li>
+            {/* Posts link  */}
+            <Link className={styles.posts} to={'/posts'}>
+              Go to All Posts
+              <span>→</span>
+            </Link>
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
       </div>
     </>
   )
