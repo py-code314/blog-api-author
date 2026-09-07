df727ed1c78beddec75ce985da89c2a415533baa
srikk <122169588+py-code314@users.noreply.github.com>
2026-09-06 22:16:16 -0400

    feat(posts): Add component to display 2 most recent posts

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import styles from './Dashboard.module.css'
 import Profile from '../../components/pages/homepage/profile/Profile'
+import RecentPosts from '../../components/pages/homepage/recent-posts/RecentPosts'
 
 const Dashboard = () => {
   return (
     <>
       <div className={styles.dashboard}>
         <Profile />
+        <RecentPosts/>
       </div>
     </>
   )
