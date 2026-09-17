551ff2b40489e9214f66728311f6089fb56d9153
srikk <122169588+py-code314@users.noreply.github.com>
2026-09-17 15:01:57 -0400

    feat(auth): Add logout & redirect logic inside `useSubmitForm` hook

--- src/hooks/useSubmitForm.js
+++ src/hooks/useSubmitForm.js
@@ -1,11 +1,20 @@
 /* -------------------- Hooks -------------------- */
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { useNavigate } from 'react-router'
+import { AuthContext } from '../contexts/auth/AuthContext'
 
 /* Hook for submitting a form */
 export const useSubmitForm = (url) => {
+  const { handleLogout } = useContext(AuthContext)
+  const navigate = useNavigate()
+
   // State variables
   const [status, setStatus] = useState('idle')
   const [data, setData] = useState(null)
+
+  // Abort controller
+  const controller = new AbortController()
+  const signal = controller.signal
 
   // Fetch data
   const fetchData = async (formData) => {
@@ -22,21 +31,49 @@ export const useSubmitForm = (url) => {
           Authorization: `Bearer ${authToken}`,
         },
         body: JSON.stringify(formData),
+        signal,
       })
 
-      const responseData = await response.json()
+      // Log out and redirect user to landing page
+      if (response.status === 401) {
+        handleLogout()
+        navigate('/')
+      }
 
-      setData(responseData)
-      setStatus('fetched')
-      return responseData
-    } catch (error) {
-      console.error
-      setData(error)
-      setStatus('failed')
+      // Assign either data or error to 'result' variable
+      let result = await response.json()
+
+      // Throw error from server instead of custom error
+      if (!response.ok) {
+        throw result
+      }
+
+      // Set result as data if fetch is successful
+      if (!signal.aborted) {
+        setData(result)
+        setStatus('fetched')
+      }
+
+      return result
+    } catch (err) {
+      console.error(err)
+
+      // Stop fetching if aborted
+      if (err.name === 'AbortError') {
+        console.log('Fetch request cancelled successfully')
+        return
+      }
+
+      // Fetch isn't successful
+      if (!signal.aborted) {
+        setData(err)
+        setStatus('failed')
+      }
+
       return null
     }
   }
-  
+
   // Return the function that actually submits form data
   return [fetchData, status, data]
 }
